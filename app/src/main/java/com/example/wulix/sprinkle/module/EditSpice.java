2e9421e053a45509027daae0d335b4a0d0d792c0
wukai <feilix98element@gmail.com>
2021-02-17 14:49:26 -0500

    New Recycler View

--- app/src/main/java/com/example/wulix/sprinkle/module/EditSpice.java
+++ app/src/main/java/com/example/wulix/sprinkle/module/EditSpice.java
@@ -1,4 +1,14 @@
 package com.example.wulix.sprinkle.module;
 
 public class EditSpice {
+
+    private int amount_teaspoons;
+
+    public int getAmount_teaspoons() {
+        return amount_teaspoons;
+    }
+
+    public void setAmount_teaspoons(int amount_teaspoons) {
+        this.amount_teaspoons = amount_teaspoons;
+    }
 }
